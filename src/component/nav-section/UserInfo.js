b1e667088ebe6372fe2de53df0feace34706cf30
Kutuzov Vitaly <kutuzov@algont.ru>
2023-05-11 11:43:13 +0300

    Создан компонент UserInfo для отображения имени, фамилии и меню пользователя, отдельно от комнонента навигационного меню.

---
+++ src/component/nav-section/UserInfo.js
@@ -0,0 +1,82 @@
+import { Box, ClickAwayListener, Menu, MenuItem, Typography } from "@mui/material"
+import { AnimatedIcon } from "../../layouts/navigator/styles";
+import { useState } from "react";
+import { Face } from "@mui/icons-material";
+import { useDispatch } from "react-redux";
+import authService from "../../service/auth.service";
+import { logout } from "../../store/session";
+import { styled, alpha } from '@mui/material/styles';
+
+const StyledAccount = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(2, 2.5),
+    borderRadius: Number(theme.shape.borderRadius) * 1.5,
+    backgroundColor: alpha(theme.palette.grey[500], 0.12),
+}));
+
+export const UserInfo = (props) => {
+    const dispatch = useDispatch();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = (event) => {
+        setAnchorEl(null);
+    };
+
+    async function handleLogout() {
+        try {
+            await authService.logout();
+            dispatch(logout());
+
+        } catch (error) {
+            console.log(error);
+            dispatch(logout());
+        }
+    }
+
+    return (
+        <StyledAccount>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ ml: 2, maxWidth: 150 }}>
+                    <Typography variant="subtitle2" sx={{ color: 'text.primary', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                        {props.firstName}
+                    </Typography>
+                    <Typography variant="subtitle2" sx={{ color: 'text.primary', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                        {props.lastName}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Пользователь
+                    </Typography>
+                </Box>
+                <AnimatedIcon
+                    sx={{ ml: 5 }}
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    id="basic-button"
+                    onClick={handleClick}
+                >
+                    <Face />
+                </AnimatedIcon>
+                <ClickAwayListener mouseEvent="onMouseDown" touchEvent="onTouchStart" onClickAway={handleClose}>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleLogout}>Выйти</MenuItem>
+                    </Menu>
+                </ClickAwayListener>
+            </Box>
+        </StyledAccount>
+    )
+}
--- src/layouts/navigator/nav.js
+++ src/layouts/navigator/nav.js
@@ -1,95 +1,22 @@
-import { styled, alpha } from '@mui/material/styles';
-import { Box, ClickAwayListener, Menu, MenuItem, Typography } from '@mui/material';
-import { useDispatch, useSelector } from 'react-redux';
-import authService from "../../service/auth.service";
-import { logout } from "../../store/session";
+import { Box, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
 import { getFirstName, getLastName, getStatusLoadingUser } from '../../store/user';
-import { useState } from 'react';
 import NavThemes from '../../component/nav-section/NavThemes';
-import { AnimatedIcon } from './styles';
-import { Face, School } from '@mui/icons-material';
+import { School } from '@mui/icons-material';
 import { LinkButton } from '../../component/LinkButton';
+import { UserInfo } from '../../component/nav-section/UserInfo';
 
 const NAV_WIDTH = 350;
 
-const StyledAccount = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(2, 2.5),
-    borderRadius: Number(theme.shape.borderRadius) * 1.5,
-    backgroundColor: alpha(theme.palette.grey[500], 0.12),
-}));
-
-
 export default function Nav() {
-    const dispatch = useDispatch();
     const firstName = useSelector(getFirstName());
     const lastName = useSelector(getLastName());
     const isLoadingUserInfo = useSelector(getStatusLoadingUser());
 
-    async function handleLogout() {
-        try {
-            await authService.logout();
-            dispatch(logout());
-
-        } catch (error) {
-            console.log(error);
-            dispatch(logout());
-        }
-    }
-
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleClose = (event) => {
-        setAnchorEl(null);
-    };
-
     const renderContent = (
         <Box>
             <Box sx={{ mb: 5, mx: 5 }}>
-                <StyledAccount>
-                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
-                        <Box sx={{ ml: 2, maxWidth: 150 }}>
-                            <Typography variant="subtitle2" sx={{ color: 'text.primary', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                {firstName}
-                            </Typography>
-                            <Typography variant="subtitle2" sx={{ color: 'text.primary', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                {lastName}
-                            </Typography>
-                            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                Пользователь
-                            </Typography>
-                        </Box>
-                        <AnimatedIcon
-                            sx={{ ml: 5 }}
-                            aria-controls={open ? 'basic-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                            id="basic-button"
-                            onClick={handleClick}
-                        >
-                            <Face />
-                        </AnimatedIcon>
-                        <ClickAwayListener mouseEvent="onMouseDown" touchEvent="onTouchStart" onClickAway={handleClose}>
-                            <Menu
-                                id="basic-menu"
-                                anchorEl={anchorEl}
-                                open={open}
-                                onClose={handleClose}
-                                MenuListProps={{
-                                    'aria-labelledby': 'basic-button',
-                                }}
-                            >
-                                <MenuItem onClick={handleLogout}>Выйти</MenuItem>
-                            </Menu>
-                        </ClickAwayListener>
-                    </Box>
-                </StyledAccount>
+                <UserInfo firstName={firstName} lastName={lastName}/>
             </Box>
             <Typography
                 variant="h6"
