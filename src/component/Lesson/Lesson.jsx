946b5b1f40533ce93cf51a8ce0509230c34f1c8d
Kutuzov Vitaly <kutuzov@algont.ru>
2023-05-30 16:39:08 +0300

    Исправил баг с отображением картинки, теперь можно позиционировать лево/центр/право

--- src/component/Lesson/Lesson.jsx
+++ src/component/Lesson/Lesson.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { getDataLesson } from "../../store/lesson";
 import "./Lesson.css";
+import { Box } from "@mui/material";
 
 export default function Lesson() {
     const { description } = useSelector(getDataLesson());
@@ -9,7 +10,7 @@ export default function Lesson() {
         <>
             {
                 description ?
-                    <div dangerouslySetInnerHTML={{ __html: description }
+                    <Box className='lessonDiv' dangerouslySetInnerHTML={{ __html: description }
                     } /> :
                     <div>Добро пожаловать в систему обучения персонала эксплуатации АСУ ИС!</div>
             }
